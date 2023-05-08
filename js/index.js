8ef9ac2261d1429f1b046c355add0e313d452de5
royalgk <jitu45290@gmail.com>
2023-05-08 11:07:16 +0530

    css,html and js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function showData(){
+    document.querySelector('img').classList.add('showimg');
+    document.querySelector('.form').classList.add('showform');
+};
+
+function colseModel(){
+    document.querySelector('img').classList.remove('showimg');
+    document.querySelector('.form').classList.remove('showform');
+}
+// var c =document.querySelector('.btn');
+// c.addEventListener('click',colseModel);
